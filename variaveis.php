24fc528ae355652370a7d2a007c530ced5ea85df
Lino Veloso <linoveloso@outlook.com>
2019-01-28 17:31:16 -0200

    Update variaveis.php

---
+++ variaveis.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Curso de PHP</title>
+	</head>
+	<body>
+	<?php 
+		//string
+		$nome = 'Lino Veloso';
+
+		//int
+		$idade = 25;
+		//float
+		$peso = 71.5;
+		//boolean
+		$fumante_sn = false;
+	 ?>
+
+	 <h1>Ficha Cadastral</h1>
+	 <br>
+	 <p> Nome: <?= $nome ?> </p>
+	 <p> Idade:<?= $idade ?> </p>
+	 <p> Peso: <?= $peso ?> </p>
+	 <p> Fumante: <?= $fumante_sn ?> </p>
+
+	</body>
+</html>
