1e49a28a8406d3ff6dda148f8998177f4d64eaf2
Lino Veloso <linoveloso@outlook.com>
2019-01-30 17:23:57 -0200

    Create concatenacao.php

---
+++ concatenacao.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Curso de PHP</title>
+	</head>
+	<body>
+	<?php
+
+	$nome = 'Lino bonitão';
+	$cor = 'azul';
+	$atividadePreferida = 'Andar de bike';
+
+		echo 'Olá '. $nome . ' , vi aqui que você gosta da cor ' . $cor . ' e você gosta de '. $atividadePreferida;
+	 ?>
+	</body>
+</html>
