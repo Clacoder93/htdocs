64d2ef11de7e2cc6acc1e5303c0a765a324be018
Lino Veloso <linoveloso@outlook.com>
2019-01-31 15:17:13 -0200

    Create variaveis_constantes.php

---
+++ variaveis_constantes.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Curso de PHP</title>
+	</head>
+	<body>
+	
+	<?php 
+		
+		define('BD_URL', 'endereco_bd_dev');
+		define('BD_USUARIO', 'usuario_dev');
+		define('BD_SENHA', 'senha_dev');
+
+		// ...
+		// lógica 
+		define('BD_URL', 'sei lá');
+
+		echo BD_URL . '<br />';
+		echo BD_USUARIO . '<br />';
+		echo BD_SENHA . '<br />';
+	?>
+
+	</body>
+</html>
