71b42a1cf33de2ed18bcb18fdb0c27f6cdcb6fff
Lino Veloso <linoveloso@outlook.com>
2019-01-31 15:57:54 -0200

    Create ifElse_operadores_condicionais.php

---
+++ ifElse_operadores_condicionais.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Curso de PHP</title>
+	</head>
+	<body>
+	
+	<?php 
+	 if (2 == '2') {
+	 	echo 'verdadeiro';
+	 } else {
+	 	echo 'falso';
+	 }
+	 
+	 ?>
+
+	
+	
+
+	</body>
+</html>
