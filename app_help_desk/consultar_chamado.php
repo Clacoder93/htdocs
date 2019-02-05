2ec9ff35854cd394b8681357563090b11e912447
Lino Veloso <linoveloso@outlook.com>
2019-02-05 18:36:48 -0200

    Require update

--- app_help_desk/consultar_chamado.php
+++ app_help_desk/consultar_chamado.php
@@ -1,10 +1,5 @@
-<?php 
-session_start();
+<? require_once "validador_acesso.php"?>
 
-if (isset($_SESSION['autenticado']) ||$_SESSION['autenticado'] != 'SIM') {
-  header('Location: index.php?login=notAuthenticated');
-}
-?>
 
 <html>
 <head>
